5da9aba141f28d7d7d5e27ea437e0d33500df1f2
Sam06tanb <78392140+sam06tanb@users.noreply.github.com>
2024-01-25 20:17:07 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] lines = new String[] {  "Dog", "Cat", "Fish"};
+
+        String path = "C:\\Users\\Fernando\\Desktop\\Arq\\Oi amigos";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
